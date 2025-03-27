6bb25662af19ce8f437c3461e425885aad6f2b08
tiansu <2056387427@qq.com>
2025-03-27 10:23:28 +0800

    refactor(main): 引入cmd_main模块并调用其main函数
    为了将命令行功能模块化，将cmd_main模块引入main.py并调用其main函数，以提高代码的可维护性和可扩展性

--- zhmm/main.py
+++ zhmm/main.py
@@ -7,6 +7,8 @@ import sys
 
 from PyQt6.QtCore import QCoreApplication
 
+import zhmm.cmd_main as cmd_main
+
 # 获取当前脚本的绝对路径，并推导出项目根目录（假设项目根目录是包含 `project` 的目录）
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(current_dir)  # 向上回退两级到项目根目录
@@ -17,6 +19,7 @@ QCoreApplication.setApplicationName("zhmm")
 QCoreApplication.setOrganizationName("szgenle")  # 替换为您的组织名称
 
 def main():
+    cmd_main.main()
     pass
 
 
