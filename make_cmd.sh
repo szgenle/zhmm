b1fb604adb1a914c4e3b03d23443f5700f3c7913
Lioesquieu <2056387427@qq.com>
2025-05-18 20:03:24 +0800

    build: 添加make_cmd.sh脚本用于构建可执行文件
    该脚本用于清理构建目录并安装pyinstaller，然后使用pyinstaller生成单文件可执行文件。脚本还指定了OS X的bundle标识符和图标。

---
+++ make_cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+rm -rf build dist
+rm -rf ./QiJie.spec
+
+poetry add pyinstaller --group dev
+#cp -rf ../zimo/resources/ ./QiJie/resources/
+#cd QiJie
+#pyinstaller --onefile --windowed --name "zhmm" --add-data "resources/*:resources"  --icon=myicon.icns zhmm/main.py --paths zhmm/
+poetry run pyinstaller --onefile  --name "zhmm" --osx-bundle-identifier "com.szgenle.zhmm" --icon=myicon.icns zhmm/cmd_main.py --paths zhmm/
+
+
